5db8fcd9538aaab92eeb413f580dd5c58b2d639b
Liam <94720916+Liam1337@users.noreply.github.com>
2022-08-25 19:12:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+/*
+     _____     ____
+    /      \  |  o | 
+   |        |/ ___\| 
+   |_________/     
+   |_|_| |_|_| v1.1
+   
+   Made by @Liam1337
+
+   Usage: https://localhost/api.php?pass=yourpassword
+   
+
+*/
+
+    error_reporting(E_ALL);
+
+    $ip = "1.1.1.1";        // Your server IP | "ip a"
+    $port = "22";           // (22 defeault SSH port), Change it if you have changed your ssh port to something else
+    $ssh_username = "root"; // server username
+    $ssh_pass = "toor";     // server passw
+
+    function EncodeString($string)
+    {
+        return htmlspecialchars($string, ENT_QUOTES, "UTF-8");
+    }
+ 
+    $pass = array("ChangeMe!"); // API passowrd
+ 
+    if (!isset($_GET["pass"]))
+    {
+        die("Please submit a password!");
+    }
+ 
+    $ssh = "service nginx stop && sleep 5 && service nginx start && sleep 5 && service nginx reload";  // Shell command for example "reboot"
+
+    $password = EncodeString($_GET["pass"]); // get passwd from the api url
+ 
+    if (!in_array($password, $pass)){ // checking pw / printing error
+        die("Invalid Password!");
+    }
+ 
+    $socket = fsockopen($ip, $port); // trying to connect
+ 
+    ($socket ? null : die("Failed to connect!")); // socket issue
+ 
+    fwrite($socket, " \r\n");
+   
+    sleep(3); // if your server is slow increase the time
+   
+    fwrite($socket, $ssh_username . "\r\n"); // server username
+   
+    sleep(3);
+   
+    fwrite($socket, $ssh_pass . "\r\n"); // user passwd
+ 
+    sleep(9);
+ 
+    fwrite($socket, $ssh); // inject shell
+ 
+    fclose($socket); // close socket
+ 
+    echo "Your command has been successfully sent to the server!"; // output if success
+?>
